9bc9ca25ff6ea9a43457c6159e461fefb51d640c
Jasmine Moore <jasminemoore818@gmail.com>
2021-02-17 18:12:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,5 +1,6 @@
 function basicTeenager(age) {
-
+if(13, 14, 15,16, 17,18, 19)
+"You are a teenager"
 }
 
 function teenager(age) {
@@ -8,12 +9,12 @@ function teenager(age) {
 
 function ageChecker(age) {
 
-}
 
 function ternaryTeenager(age) {
-
+if(16) "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
-
+if (12)
+"You have an age"
 }
